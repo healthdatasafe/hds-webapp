c9f7312f3c319936f01557aca38e2a4fc5232891
Perki <perki@pryv.com>
2025-05-14 18:27:42 +0200

    Adding backloop.dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import backloop from 'vite-plugin-backloop.dev';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const config = {
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === 'development' &&
+      componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
+  if (mode === 'backloop') {
+    config.plugins.push(backloop('mycomputer'));
+  }
+  return config;
+});
